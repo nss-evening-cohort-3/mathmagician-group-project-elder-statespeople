c5123d6586ba91eab8f9a34f7fdba5dbe70deed6
Dhru Upender <dhruti.upender@gmail.com>
2016-08-01 21:21:24 -0500

    Unit test project setup and first Integer Test

--- Mathmagician.Tests/UnitTest1.cs
+++ Mathmagician.Tests/UnitTest1.cs
@@ -6,9 +6,31 @@ namespace Mathmagician.Tests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        public void ThisIsNotATest()//all other methods can use this one
         {
+
+        }
+
+        [TestMethod]//This must be annotated, required for VS to view it as a test and put it in Test Explorer
+        public void TestThatThisIsTrue()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ThisShouldFail()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Fail();
         }
     }
 }
