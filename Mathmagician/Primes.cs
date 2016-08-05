62f3e5b6c7d3fab47a2a0d5c18bd86078b3afe16
Scott Schaffer <scottpschaffer@hotmail.com>
2016-08-04 18:47:50 -0700

    finished Program.cs and Primes.cs

---
+++ Mathmagician/Primes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Primes
+    {
+        public int[] GeneratePrimes(int count)
+        {
+            int[] c = new int[count];
+
+            Console.WriteLine("The number of Prime Numbers I will return is " + count);
+
+            // Counter for # of successful Prime #s
+            int loopCounter = 0;
+            // Number being checked is Prime
+            int numCheck = 1;
+            // While haven't reached the number of desired Primes
+            while (loopCounter < count)
+            {
+                // Increment the number being checked
+                numCheck++;
+                // If Number is Prime or not
+                bool primeNum = true;
+                
+                // Starts at 2 and goes to 1 less than the number being checked
+                for (int j = 2; j < numCheck; j++)
+                {
+                    // If number being checked is divisible by a number
+                    if ((numCheck % j) == 0)
+                    {
+                        // Not a Prime number if divisible
+                        primeNum = false;
+                    }
+                }
+                // If Prime Number (including 2)
+                if ((primeNum) || (numCheck == 2))
+                {
+                    c[loopCounter] = numCheck;
+                    // Increment count of Prime Numbers
+                    loopCounter++;
+                }
+            }
+            return c;
+        }
+
+        public string PrintPrimes(int[] primeNums)
+        {
+            string output = "";
+            foreach (int p in primeNums)
+            {
+                output += " " + p + " ";
+            }
+            return output;
+        }
+        
+    }
+}
--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -8,8 +8,71 @@ namespace Mathmagician
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            bool loopCount = true;
+
+            while (loopCount)
+            {
+                Console.WriteLine("What would you like for me to do?");
+                string command = Console.ReadLine();
+                
+                switch (command)
+                {
+                 /*   case "Integers":
+                        HowMany hmInt = new HowMany();
+                        Integers i = new Integers();
+                        int numberOfInts = hmInt.GetNumber();
+                        int[] ints = i.GenerateIntegers(numberOfInts);
+                        string outputInts = i.PrintIntegers(ints);
+                        Console.WriteLine("The " + numberOfInts + " Integers are: " + outputInts);
+                        loopCount = false;
+                        break; */
+                    case "Primes":
+                        HowMany hmPrimes = new HowMany();
+                        Primes p = new Primes();
+                        int numberOfPrimes = hmPrimes.GetNumber();
+                        int[] primes = p.GeneratePrimes(numberOfPrimes);
+                        string outputPrimes = p.PrintPrimes(primes);
+                        Console.WriteLine("The " + numberOfPrimes + " Prime Numbers are: "  + outputPrimes);
+                        loopCount = false;
+                        break;
+                   /* case "Fibonacci":
+                        HowMany hmFibs = new HowMany();
+                        Fibonacci f = new Fibonacci();
+                        int numberOfFibs = hmFibs.GetNumber();
+                        int[] fibs = f.GenerateFibs(numberOfFibs);
+                        string outputFibs =f.PrintFibs(fibs);
+                        Console.WriteLine("The " + numberOfFibs + " Fibonacci Numbers are: " + outputFibs);
+                        loopCount = false;
+                        break;
+                    case "Even":
+                        HowMany hmEvens = new HowMany();
+                        Evens e = new Evens();
+                        int numberOfEvens = hmEvens.GetNumber();
+                        int[] evens = e.GenerateEvens(numberOfEvens);
+                        string outputEvens = e.PrintEvens(evens);
+                        Console.WriteLine("The " + numberOfEvens + " Even Numbers are: " + outputEvens);
+                        loopCount = false;
+                        break;
+                    case "Odd":
+                        HowMany hmOdds = new HowMany();
+                        Odds o = new Odds();
+                        int numberOfOdds = hmOdds.GetNumber();
+                        int[] odds = o.GenerateOdds(numberOfOdds);
+                        string outputOdds = o.PrintOdds(odds);
+                        Console.WriteLine("The " + numberOfOdds + " Prime Numbers are: " + outputOdds);
+                        loopCount = false;
+                        break; */
+                    default:
+                        Console.WriteLine("Whoops!");
+                        break;
+                }
+             
+            }
+            Console.WriteLine("Hit Enter key to finish");
+            Console.Read();
         }
     }
 }
