d4f5fe048029bca5f2f348dad12503fafa99a9f4
Jurnell Cockhren <jcockhren@users.noreply.github.com>
2016-08-05 23:01:39 -0500

    Revert "Primes"

--- Mathmagician.Tests/UnitTest1.cs
+++ Mathmagician.Tests/UnitTest1.cs
@@ -6,31 +6,9 @@ namespace Mathmagician.Tests
     [TestClass]
     public class UnitTest1
     {
-        public void ThisIsNotATest()//all other methods can use this one
-        {
-
-        }
-
-        [TestMethod]//This must be annotated, required for VS to view it as a test and put it in Test Explorer
-        public void TestThatThisIsTrue()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-            Assert.IsTrue(true);
-        }
-
         [TestMethod]
-        public void ThisShouldFail()
+        public void TestMethod1()
         {
-            //Arrange
-
-            //Act
-
-            //Assert
-            Assert.Fail();
         }
     }
 }
